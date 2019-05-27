3eeeff34f7266b1346b22f75b25968144f0ebbb3
Ichen.tsu <r99521320@gmail.com>
2019-05-27 17:12:08 +0800

    Text_Justification_68 submit

--- src/Text_Justification_68.java
+++ src/Text_Justification_68.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Text_Justification_68 {
 
-    public static void main(String[] args) {
+    public static void main(String[] arg0) {
 
-//        List<String> words = new ArrayList<String>() ;
+        //        List<String> words = new ArrayList<String>() ;
         String[] words = new String[7];
         words[0] = "This";
         words[1] = "is";
@@ -13,29 +13,60 @@ public class Text_Justification_68 {
         words[4] = "of";
         words[5] = "text";
         words[6] = "justification";
-        System.out.println(fullJustify(words, 20));
+        result(words, 16);
     }
 
-//    private static HashMap<String, Integer> resultMap = new HashMap<>() ;
+    private static void result(String[] words, int L) {
+        List<String> ans = new ArrayList<String>();
 
-    public static List<String> fullJustify(String[] words, int maxWidth) {
-
-
-
-
-
-        List<String> result = new ArrayList<>();
-        for(int index = 0; index < words.length; index ++) {
-            if (index == 0) {
-                result.add("\n" + "   \"" + words[index] + "\"");
-            } else if (index == words.length - 1) {
-                result.add("\n" + "   \"" +  words[index] + "\"\n");
-            } else {
-                result.add("\n" + "   \"" +  words[index] + "\"");
+        int n = words.length;
+        int i = 0;
+        while (i < n) {
+            int len = words[i].length();
+            int j = i + 1;
+            while (j < n && len + 1 + words[j].length() <= L) {
+                len += 1 + words[j].length();
+                j++;
             }
-        }
+            System.out.println("i= " + i + ", j= " + j + ", n= " + n);
 
-        return result;
+            String line = words[i];
+            if (j == n) { // if this is the last line, the corner case.
+                for (int k = i + 1; k < n; k++) {
+                    line += " " + words[k];
+                }
+                while (line.length() < L) {
+                    line += " ";
+                }
+            } else {
+                int extraWhite = L - len;
+                int whiteNum = j - i - 1;
+                System.out.println("extraWhite= " + extraWhite);
+                System.out.println("whiteNum= " + whiteNum);
+
+                if (whiteNum == 0) { // if this line has only one word
+                    while (line.length() < L) {
+                        line += " ";
+                    }
+                } else {
+                    for (int k = i + 1; k < j; k++) {
+                        line += " ";
+                        for (int p = 0; p < extraWhite/whiteNum; p++) {
+                            line += " ";
+                        }
+                        if (k - i <= extraWhite%whiteNum) {
+                            line += " ";
+                        }
+                        line += words[k];
+                    }
+                }
+            }
+            ans.add(line);
+
+            i = j;
+        }
+        System.out.println(ans);
+//        return ans;
     }
 
 }
